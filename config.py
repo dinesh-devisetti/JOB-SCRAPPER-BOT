7f9afd413818a3710d45c66b3128a42cf0fd38bd
dinesh1816 <ddinesh1816@gmail.com>
2025-12-16 12:22:31 -0600

    made changes to jobs

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
-EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")  # TODO: Define this in GitHub Secrets
-EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")  # TODO: Define this in GitHub Secrets
+EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+
 
 if not EMAIL_ADDRESS or not EMAIL_PASSWORD:
     raise ValueError("Please set EMAIL_ADDRESS and EMAIL_PASSWORD environment variables.")
