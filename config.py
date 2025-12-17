9b0c99793e9721363506a7c33079e79fb15b8cb8
dinesh1816 <ddinesh1816@gmail.com>
2025-12-17 11:33:46 -0600

    error fix and cron timing changed

--- config.py
+++ config.py
@@ -2,7 +2,3 @@ import os
 
 EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
-
-
-if not EMAIL_ADDRESS or not EMAIL_PASSWORD:
-    raise ValueError("Please set EMAIL_ADDRESS and EMAIL_PASSWORD environment variables.")
